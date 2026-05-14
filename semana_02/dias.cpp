94d72b598f06103b7b9c5c91365f57f5f86544a5
yago66 <ysajami6@gmail.com>
2026-05-14 15:21:25 -0500

    Add program to display day of the week

---
+++ semana_02/dias.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int dia;
+   cout <<"ingrese  un numero del 1 al 7";
+   cin>>dia;
+   switch(dia){
+    case 1:
+   	cout<<"lunes, laborable";
+   	break;
+   	
+   	case 2:
+   	cout<<"Martes, laborable";
+   	break;
+   	
+   	case 3:
+   	cout <<"Miercoles, laborable";
+   	break;
+   	
+   	case 4:
+   	cout<<"Jueves, laborable";
+   	break;
+   	
+   	case 5:
+   	cout <<"Viernes, laborable";
+   	break;
+   	
+   	case 6:
+   	cout<<"Sabado, laborable";
+   	break;
+   	
+   	case 7:
+   	cout<<"Domingo, NO LABORABLE";
+   	break;
+   	
+   	default:
+   	cout <<"numero invalido";
+  }
+}
